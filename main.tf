205c762a713552d13e076c44f08f53f6498f68d1
BassaBN <130400884+BassaBN@users.noreply.github.com>
2023-05-05 04:27:18 +0800

    QQQ

--- main.tf
+++ main.tf
@@ -1,13 +1,5 @@
-terraform {
-  required_providers {
-    aws = {
-      source  = "hashicorp/aws"
-      version = "~> 4.0"
-    }
-  }
-}
-
 provider "aws" {
+  version = "~> 2.7"
   region  = var.aws_region
 }
 
